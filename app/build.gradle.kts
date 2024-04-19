313ac5abb9e0357c101894a1aadd323830736713
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2024-04-18 23:24:10 -0500

    Login Feature is Complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,10 +38,17 @@ android {
 
 dependencies {
 
+    // Google sign in dependencies please don't remove
+    implementation("com.google.firebase:firebase-auth:19.4.0")
+    implementation("com.google.android.gms:play-services-auth:18.1.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.8.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/ihomie/SignUpActivity.kt
@@ -0,0 +1,75 @@
+package com.example.ihomie
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signupEmail: EditText
+    private lateinit var signupPassword: EditText
+    private lateinit var signupButton: Button
+    private lateinit var loginRedirectText: TextView
+    private lateinit var googleSignUpBtn: SignInButton // Add Google SignInButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+        signupEmail = findViewById(R.id.signup_email)
+        signupPassword = findViewById(R.id.signup_password)
+        signupButton = findViewById(R.id.signup_button)
+        loginRedirectText = findViewById(R.id.loginRedirectText)
+//        googleSignUpBtn = findViewById(R.id.googleSignUpBtn) // Initialize Google SignInButton
+
+        signupButton.setOnClickListener {
+            val user = signupEmail.text.toString().trim { it <= ' ' }
+            val pass = signupPassword.text.toString().trim { it <= ' ' }
+            if (user.isEmpty()) {
+                signupEmail.error = "Email cannot be empty"
+            }
+            if (pass.isEmpty()) {
+                signupPassword.error = "Password cannot be empty"
+            } else {
+                auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "SignUp Successful",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        startActivity(
+                            Intent(
+                                this@SignUpActivity,
+                                LoginActivity::class.java
+                            )
+                        )
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "SignUp Failed" + task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+        loginRedirectText.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@SignUpActivity,
+                    LoginActivity::class.java
+                )
+            )
+        }
+    }
+}
