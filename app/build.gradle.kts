d5f2c529551d0fc61947f273c33c0cc632c10729
Natasha <wong.natasha.m@gmail.com>
2024-04-25 09:05:14 -0700

    Edited Statistics xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.preference:preference:1.2.1")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation ("androidx.fragment:fragment-ktx:1.5.0")
+    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
